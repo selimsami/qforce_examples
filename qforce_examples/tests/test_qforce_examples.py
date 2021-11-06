77b3de4e5e84c3128b289cc8e58220659f7efa63
Zhiyi Wu <zhiyi.wu@bioch.ox.ac.uk>
2021-11-06 18:27:21 +0000

    Update test_qforce_examples.py

--- qforce_examples/tests/test_qforce_examples.py
+++ qforce_examples/tests/test_qforce_examples.py
@@ -3,13 +3,13 @@ Unit and regression test for the qforce_examples package.
 """
 
 # Import package, test suite, and other packages as needed
-import sys
-
+import os
 import pytest
 
-import qforce_examples
+from qforce_examples import Orca_default, Gaussian_default
 
-
-def test_qforce_examples_imported():
-    """Sample test, will always pass so long as import statement worked."""
-    assert "qforce_examples" in sys.modules
+@pytest.mark.parametrize("test_input", [Orca_default, Gaussian_default])
+def test_files_exist(test_input):
+    """Test if the files do exist."""
+    for key in test_input:
+        assert os.path.isfile(test_input[key])
